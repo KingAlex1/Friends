2db767da6187b3487777d7a9c4c51e8a3738c9c1
Alex Bogomolov <alex-sert2015@mail.ru>
2017-09-21 23:02:10 +0400

    сделал пкоректную работу dnd

--- src/app.js
+++ src/app.js
@@ -34,18 +34,85 @@ promise
         const [user] = data;
     //    headerInfo.innerText = `${user.first_name} ${user.last_name}`;
 
-        return api ('friends.get',{v:5.68,fields:'first_name, last_name, photo_100'})
+        return api ('friends.get',{v:5.68,fields:'first_name, last_name, photo_100 , id'})
     })
     .then(data=>{
         const templateElement = document.querySelector('#user-template');
         const source = templateElement.innerHTML,
             templateFn = Handlebars.compile(source),
             template = templateFn({list: data.items});
+        return results.innerHTML = template;
 
-        results.innerHTML = template;
     })
+   /* .then(()=>{
+        var cols = document.querySelectorAll('.draggable-div');
+        [].forEach.call(cols, function(col) {
+            col.addEventListener('dragstart', handleDragStart, false);
+            col.addEventListener('dragover', handleDragOver, false);
+            col.addEventListener('drop', handleDrop, false);
+        });
+    })
+*/
+   /*.then(()=>{
+    function dragDrop(ev) {
+    var data = ev.dataTransfer.getData("Text");
+    var enemNode = document.querySelectorAll(' draggable-div');
+    var elem = document.querySelector('.draggable-div');
+
+    this.addEventListener('mouseup',function (e) {
+    if( e.target !== enemNode ){
+    ev.stopPropagation();
+    }
+    ev.target.appendChild(document.getElementById(data));
+    })
+
+
+
+    return false;
+    }
+    })*/
+
+
     .catch(function (e) {
         alert('Ошибка:' + e.message);
 
     });
 
+////////////////////////////////////////
+////////////////////////////////////////
+////////////draganddrop/////////////////
+////////////////////////////////////////
+////////////////////////////////////////
+function dragStart(ev) {
+    ev.dataTransfer.effectAllowed='move';
+    ev.dataTransfer.setData("Text", ev.target.getAttribute('id'));
+    ev.dataTransfer.setDragImage(ev.target,150,30);
+    return true;
+}
+function dragEnter(ev) {
+    ev.preventDefault();
+    return true;
+}
+function dragOver(ev) {
+    ev.preventDefault();
+}
+function dragDrop(ev) {
+    var data = ev.dataTransfer.getData("Text");
+    var elem = document.querySelector('.app__choose_friend-list');
+    elem.appendChild(document.getElementById(data));
+    ev.stopPropagation();
+    return false;
+}
+function dragDropBack (ev) {
+    var data = ev.dataTransfer.getData("Text");
+    var elemResult = document.querySelector('.app__choose_result');
+
+    if(ev.target.classList.contains("app__choose_users")){
+        elemResult.appendChild(document.getElementById(data))
+            }
+
+
+    elemResult.appendChild(document.getElementById(data));
+    ev.stopPropagation();
+    return true;
+}
